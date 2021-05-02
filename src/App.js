e109f3abdcc7d3de1843402e8d852b693e6f5efb
duwls485 <duwls485@gmail.com>
2021-05-03 01:48:30 +0900

    5강

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer'
 
+const customer = [
+  {
+    'id':1,
+    'image':'https://placeimg.com/64/64/1',
+    'name' : "홍길동1",
+    'birthday':'961212',
+    'gender':'남자',
+    'job' : '대학생'
+  },
+  {
+    'id':2,
+    'image':'https://placeimg.com/64/64/2',
+    'name' : "홍길동2",
+    'birthday':'961212',
+    'gender':'남자',
+    'job' : '대학생'
+  },
+  {
+    'id':3,
+    'image':'https://placeimg.com/64/64/3',
+    'name' : "홍길동3",
+    'birthday':'961212',
+    'gender':'남자',
+    'job' : '대학생'
+  }
+]
 function App() {
   return (
-   <div className="gray-background">
-     <img src={logo} lat="logo"/>
-     <h2> let's develop management system</h2>
-   </div>
+    <div>
+      {
+        customer.map(c=>{
+          return (
+            
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.gender}
+              job={c.job}
+            />
+              
+          )
+        })
+      }
+    </div>
   );
 }
 
